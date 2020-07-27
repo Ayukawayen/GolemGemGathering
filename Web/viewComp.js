198eac98f871765203d948112f00b8ec1d4fd23b
Ayukawayen <Ayukawayen@Gmail.com>
2020-07-27 16:29:33 +0800

    feat(Web): remove network selector on nav

--- Web/viewComp.js
+++ Web/viewComp.js
@@ -2,25 +2,14 @@
 
 Vue.component('navComp', {
 	props: ['links'],
-	data: function() {
-		return {
-			networks: ['Ropsten', 'Goerli'],
-		};
-	},
 	
 	template: '<nav>'
 		+ '<slot v-for="(item,i) in links" :key="i">'
 		+ 	'<a :href="item.href">{{ item.text }}</a>'
 		+ 	'<span v-if="i<links.length-1"> / </span>'
 		+ '</slot>'
-		+ '<select class="network" @change="onNetworkChange" :value="ContractMetadata.networkName">'
-		+ 	'<option v-for="(item,i) in networks" :key="i">{{ item }}</option>'
-		+ '</select>'
 	+ '</nav>',
 	
 	methods: {
-		onNetworkChange: function(ev){
-			NetworkData.setNetwork(ev.target.value);
-		},
 	},
 })
